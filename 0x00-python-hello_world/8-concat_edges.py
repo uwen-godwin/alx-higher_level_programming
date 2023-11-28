6429e0c057cb667df5b1a6030c5028eac30260a1
uwen-godwin <uwengodwin001@gmail.com>
2023-11-27 17:32:42 -0800

    Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 language ithat combines remarkable power with very clear syntax"
-print(str[39:66] + ' ' + str[-22:-18] + ' ' + str[:6])
+str = (str[39:66] + ' ' + str[-22:-18] + ' ' + str[:6])
 print(str)
