65b9d3a2a1220bf51839cd1ce052d4238c197290
uwen-godwin <uwengodwin001@gmail.com>
2023-12-19 17:06:56 -0800

    A Python function def magic_calculation(a, b):

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,11 @@
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 3):
+    for i in range(1, 4):
         try:
             if i > a:
-                raise Exception('Too far')
+                raise ValueError('Too far')
             result += a ** b / i
-        except:
+        except ValueError:
             result = b + a
             break
     return result
