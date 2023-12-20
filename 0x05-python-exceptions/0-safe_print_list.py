16d2cfc778a840b62778dfb73137b2297cc8149e
uwen-godwin <uwengodwin001@gmail.com>
2023-12-19 16:20:53 -0800

    A function that executes a function safely.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
@@ -12,6 +11,7 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         print()
         return count
+
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
