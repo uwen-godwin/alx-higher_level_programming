7311f5b95be41d040ccf5b1981c87f4552f8bd25
uwen-godwin <uwengodwin001@gmail.com>
2023-12-19 16:59:12 -0800

    A function that prints an integer.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as e:
-        print("Exception: {}".format(e))
+        print("Exception:", e, file=sys.stderr)
         return False
+
 
 if __name__ == "__main__":
     value = 89
