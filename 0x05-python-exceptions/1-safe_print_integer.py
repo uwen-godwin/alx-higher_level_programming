e7b146e89a9655c68a2c8687e837ba08458ca622
uwen-godwin <uwengodwin001@gmail.com>
2023-12-19 16:24:43 -0800

    A function that prints an integer with {:d}.format().

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,7 @@ def safe_print_integer(value):
     except (ValueError, TypeError):
         return False
 
+
 if __name__ == "__main__":
     value = 89
     has_been_print = safe_print_integer(value)
