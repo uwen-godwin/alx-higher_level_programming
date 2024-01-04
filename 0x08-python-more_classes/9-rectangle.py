99a0991ea152b25ca4e75cc7be0019598cc4ddd6
uwen-godwin <uwengodwin001@gmail.com>
2024-01-04 07:14:16 -0800

    A class Rectangle that defines a rectangle by: (based on 8-rectangle.py)

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-"""Module for Rectangle class."""
-from typing import List
-
-
 class Rectangle:
-    """A class that represents a rectangle."""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def area(self):
+        return self.width * self.height
 
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        result = ''
+        for i in range(self.height):
+            result += '#' * self.width
+            if i < self.height - 1:
+                result += '\n'
+        result += '\nBye rectangle...'
+        return result
+
+    @classmethod
+    def square(cls, size):
+        return cls(size, size)
