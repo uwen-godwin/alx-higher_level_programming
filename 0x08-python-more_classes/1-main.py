a51ce8b1a05afe14f30972eefd2dd3c58be9fea2
uwen-godwin <uwengodwin001@gmail.com>
2024-01-04 20:49:36 -0800

    A class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 Rectangle = __import__('1-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
+my_rectangle = Rectangle(4, 2)
 print(my_rectangle.__dict__)
 
-my_rectangle.width = 10
-my_rectangle.height = 3
+my_rectangle.width = 3
+my_rectangle.height = 10
 print(my_rectangle.__dict__)
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
 
 class Rectangle:
+    """Class that defines a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize the rectangle with width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Retrieve the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
+        """Retrieve the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def __repr__(self):
-        return "{'_Rectangle__height': " + str(self.__height) + ", '_Rectangle__width': " + str(self.__width) + "}"
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle(2, 4)
-    print(my_rectangle)
-
-    my_rectangle.width = 10
-    my_rectangle.height = 3
-    print(my_rectangle)
+        else:
+            self.__height = value
