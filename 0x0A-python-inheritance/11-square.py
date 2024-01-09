f1224360e8a71377abc78f7079c244259467ee3a
uwen-godwin <uwengodwin001@gmail.com>
2024-01-08 21:30:42 -0800

    updated

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
-    """Class representing a square with a size."""
+    """Represent a square."""
 
     def __init__(self, size):
-        """Initializes a Square instance with size."""
-        super().__init__(size, size)
+        """Initialize a new square.
 
-    def __str__(self):
-        """Returns the string representation of the square."""
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
