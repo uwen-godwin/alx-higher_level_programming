88300780f3b5d32802c093164cb5631e71020b00
uwen-godwin <uwengodwin001@gmail.com>
2024-01-08 21:34:01 -0800

    updated

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
 
 class MyInt(int):
-    """Class representing MyInt that inherits from int with inverted == and != operators."""
+    """Invert int operators == and !=."""
 
-    def __eq__(self, other):
-        """Inverts the equality operator ==."""
-        return super().__ne__(other)
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-    def __ne__(self, other):
-        """Inverts the inequality operator !=."""
-        return super().__eq__(other)
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
