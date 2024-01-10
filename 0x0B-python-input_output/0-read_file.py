282a43d187997530667bee8dd43b87b40ccf9738
uwen-godwin <uwengodwin001@gmail.com>
2024-01-10 04:00:29 -0800

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to be read.
-
-    Returns:
-        None
-    """
-    with open(filename, encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
-
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
