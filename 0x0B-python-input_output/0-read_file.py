bdcbe973f966543b62aa589e3dbad96492e87fa9
uwen-godwin <uwengodwin001@gmail.com>
2024-01-10 09:18:05 -0800

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """Defines a text file-reading function."""
 
-
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    try:
+        with open(filename, mode="r", encoding="utf-8") as f:
+            print(f.read())
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
