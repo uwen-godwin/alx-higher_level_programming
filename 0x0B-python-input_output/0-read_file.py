7cdfbb6de81ebfa148db40d7822efe1c5ff1ea20
uwen-godwin <uwengodwin001@gmail.com>
2024-01-10 09:20:20 -0800

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,3 +10,8 @@ def read_file(filename=""):
         print(f"Error: File '{filename}' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    print("Script is about to be executed.")
+    read_file("my_file_0.txt")
+    print("Script execution completed.")
