59c5b964fb78e4a5aa147ef573c3350ee27a400e
uwen-godwin <uwengodwin001@gmail.com>
2024-02-15 23:10:25 -0800

    A script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,12 +1,5 @@
--- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
--- uses a database to list all rows not linked to one row
-SELECT title
-FROM tv_shows
-WHERE title NOT IN
-(SELECT title
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy')
-GROUP BY title
-ORDER BY title ASC;
+-- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
