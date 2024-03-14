61149af807ca7e63abe4be7086f6d485e04241d7
uwen-godwin <uwengodwin001@gmail.com>
2024-03-14 01:27:11 -0700

    A Update this script by adding a new function incr that increments the integer value

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,13 +2,13 @@
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr() {
-    this.value++;
-  }
+  value: 12
 };
 
 console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
