737ba5f6356b9a4008011d0f481159e995d12d9e
uwen-godwin <uwengodwin001@gmail.com>
2024-03-17 03:56:53 -0700

    Ascript that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-
-const size = process.argv[2];
-if (isNaN(size)) {
-  console.log("Missing size");
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
   }
 }
