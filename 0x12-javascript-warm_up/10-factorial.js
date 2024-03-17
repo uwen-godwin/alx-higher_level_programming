806d8c939ed7d777c0a358dc04c28857c6ba24fe
uwen-godwin <uwengodwin001@gmail.com>
2024-03-17 03:59:32 -0700

    A script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-function factorial(num) {
-    if (num === 0 || isNaN(num)) {
-        return 1;
-    }
-    return num * factorial(num - 1);
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
 }
-console.log(factorial(parseInt(process.argv[2])));
+
+console.log(factorial(Number(process.argv[2])));
