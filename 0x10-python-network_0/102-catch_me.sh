29e72b8c8fffda6b606ddca5bdcba47416f9f6cc
uwen-godwin <uwengodwin001@gmail.com>
2024-03-28 19:01:05 -0700

    python network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#makes a request to 0.0.0.0:5000/catch_me to return "You got me!"
-curl -sL -X PUT -H  0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me to return "You got me!"
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
