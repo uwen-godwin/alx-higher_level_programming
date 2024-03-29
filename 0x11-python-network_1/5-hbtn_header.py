1a4efd4b93b36684836e2f1e2cdda82a11f9f2cf
uwen-godwin <uwengodwin001@gmail.com>
2024-03-28 17:35:40 -0700

    hbtn header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id in the response header
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers.get("X-Request-Id"))
