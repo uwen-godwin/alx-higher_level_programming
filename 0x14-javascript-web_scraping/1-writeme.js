135a89f5f9383b43986529c71fd587f8f7372052
uwen_godwin <uwengodwin001@gmail.com>
2024-04-23 13:41:23 +0100

    javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,7 +8,5 @@ const content = process.argv[3];
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log('The file has been saved!');
 });
