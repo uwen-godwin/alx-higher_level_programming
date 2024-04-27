69096b711d611b4a73de98539187c103de937a81
uwen_godwin <uwengodwin001@gmail.com>
2024-04-27 08:08:07 +0100

    javascript web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,4 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Content has been saved to ${filePath}`);
-    process.exit(0);
-  });
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
