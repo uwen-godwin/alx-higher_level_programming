d97e7166736c7a8270595a01ef24f34e985de894
uwen_godwin <uwengodwin001@gmail.com>
2024-04-27 08:07:00 +0100

    javascript web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,20 @@
 #!/usr/bin/node
-
-// Import the 'request' module
+// Number of films with the given character ID
 const request = require('request');
+let num = 0;
 
-// Use the 'request' module to perform an HTTP GET request to the URL
-request(process.argv[2], function (error, response, body) {
-  // Check if there was no error during the HTTP request.
-  if (!error) {
-    // Parse the JSON data and extract the "results" array
-    const results = JSON.parse(body).results;
-    // Use the 'reduce()' method to iterate through the movies in the 'results' array.
-    console.log(results.reduce((count, movie) => {
-      // Check if there is a character with ID 18 ('/18/') in the 'characters' array.
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        // If a character with ID 18 is found, increment the count by 1.
-        ? count + 1
-        // Otherwise, keep the count unchanged.
-        : count;
-      // The 'reduce()' method starts with an initial value of 0 ('0' at the end).
-    }, 0));
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    content.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(18)) {
+          num += 1;
+        }
+      });
+    });
+    console.log(num);
   }
 });
