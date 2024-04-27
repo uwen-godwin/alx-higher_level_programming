ba44ba7fd03dc818347800418404968504e763fb
uwen_godwin <uwengodwin001@gmail.com>
2024-04-27 08:08:50 +0100

    javascript web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const tasks = JSON.parse(body);
-  const completedTasksByUser = tasks.reduce((result, task) => {
-    if (task.completed) {
-      result[task.userId] = (result[task.userId] || 0) + 1;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
     }
-    return result;
-  }, {});
-  console.log(completedTasksByUser);
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
